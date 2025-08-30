0e01421514c5c44d3f9fab93250943852b9c6dab
NicolasRamosDamiano <113316784+NicolasRamosDamiano@users.noreply.github.com>
2025-08-30 19:23:17 -0300

    desafio
    -----

--- js/verify.js
+++ js/verify.js
@@ -1,3 +1,24 @@
 if (localStorage.getItem("isLoggedIn") !== "true") {
     window.location.href = "login.html";
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const usuarioDiv = document.getElementById("usuario");
+  const username = localStorage.getItem("currentUser");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (isLoggedIn === "true" && username) {
+    usuarioDiv.innerHTML = `
+      Hola, <strong>${username}</strong> 
+      <button id="logout" style="margin-left: 10px;" class="btn btn-sm btn-outline-danger">Cerrar sesión</button>
+    `;
+
+    document.getElementById("logout").addEventListener("click", () => {
+      localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("currentUser");
+      window.location.href = "login.html";
+    });
+  } else {
+    
+    window.location.href = "login.html";
+  }
+});
