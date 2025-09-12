e4cd03eb016943d5d387b9af11f4e95a900cfaad
Kimberly2309 <kimberlyperez.2309@gmail.com>
2025-09-12 19:16:52 -0300

    arreglos

--- js/products.js
+++ js/products.js
@@ -10,9 +10,9 @@ const clearFilterBtn = document.getElementById("clearFilterBtn");
 
 const url = `https://japceibal.github.io/emercado-api/cats_products/${categoriaId}.json`;
 
+let productos = [];
+let productosFiltrados = [];
 
-let productos = [];        
-let productosFiltrados = []; 
 fetch(url)
   .then(res => res.json())
   .then(data => {
@@ -41,42 +41,13 @@ function mostrarProductos(lista) {
       <p class="producto-precio"><strong>Precio:</strong> ${producto.currency} ${producto.cost}</p>
       <p class="producto-vendido"><strong>Vendidos:</strong> ${producto.soldCount}</p>
     `;
+
+    // evento para ir a la info del producto específico
+    card.addEventListener("click", () => {
+      localStorage.setItem("productID", producto.id);
+      window.location.href = "product-info.html";
+    });
+
     contenedor.appendChild(card);
   });
-}
-
-sortAscBtn.addEventListener("click", () => {
-  productosFiltrados.sort((a, b) => a.cost - b.cost);
-  mostrarProductos(productosFiltrados);
-});
-
-sortDescBtn.addEventListener("click", () => {
-  productosFiltrados.sort((a, b) => b.cost - a.cost);
-  mostrarProductos(productosFiltrados);
-});
-
-filterPriceBtn.addEventListener("click", () => {
-  aplicarFiltros();
-});
-
-clearFilterBtn.addEventListener("click", () => {
-  minPriceInput.value = "";
-  maxPriceInput.value = "";
-  searchInput.value = "";
-  productosFiltrados = [...productos];
-  mostrarProductos(productosFiltrados);
-});
-
-function aplicarFiltros() {
-  const texto = searchInput.value.toLowerCase();
-  const precioMin = parseFloat(minPriceInput.value) || 0;
-  const precioMax = parseFloat(maxPriceInput.value) || Infinity;
-
-  productosFiltrados = productos.filter(p => {
-    const cumpleTexto = p.name.toLowerCase().includes(texto);
-    const cumplePrecio = p.cost >= precioMin && p.cost <= precioMax;
-    return cumpleTexto && cumplePrecio;
-  });
-
-  mostrarProductos(productosFiltrados);
 }
